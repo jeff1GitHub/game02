bd208e193a40534ff17105b1ef0ae62e657c1dce
hermes <hermes@aa.com>
2017-12-30 03:24:30 -0400

    aa

--- Game/ReportServer/src/main/java/com/sf/report/controller/TestController.java
+++ Game/ReportServer/src/main/java/com/sf/report/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+	
 	@RequestMapping("/hi")
 	public String hi(@RequestParam String name){
 		return "hi " + name;

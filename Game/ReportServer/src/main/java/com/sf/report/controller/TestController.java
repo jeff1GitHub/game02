227deaf2e89972a85c3f16aefc133e44f8aa06cb
hermes <hermes@aa.com>
2017-12-30 09:40:11 -0400

    增加日志配置文件

--- Game/ReportServer/src/main/java/com/sf/report/controller/TestController.java
+++ Game/ReportServer/src/main/java/com/sf/report/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.sf.report.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +10,18 @@ import com.google.gson.JsonObject;
 
 @RestController
 public class TestController {
+	private final Logger logger = LoggerFactory.getLogger(TestController.class);
 	
 	@RequestMapping("/hi")
 	public String hi(@RequestParam String name){
 		if("hermes".equals(name)){
+			logger.info("account {}", name);
 			JsonObject json = new JsonObject();
 			json.addProperty("status", 200);
 			json.addProperty("result", "hi " + name);
 			return json.toString();
 		}else{
+			logger.error("account error. {}", name);
 			throw new RuntimeException("account error.");
 		}
 	}
